79ad7ccaa6f1db0af28928f8deb11580baced131
Rayan Bassetti <r_bassetti@etu-webschoolfactory.fr>
2020-11-13 11:23:13 +0100

    update : added icons to tabBarBottom

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Search from '../components/Search'
 import Favorites from '../components/Favorites'
 import FilmDetail from '../components/FilmDetail'
+import { Image, StyleSheet } from 'react-native';
 
 // https://www.youtube.com/watch?v=nQVCkqvU1uE
 
@@ -11,13 +12,39 @@ const SearchStack = createStackNavigator()
 const FavorisStack = createStackNavigator()
 const Tabs = createBottomTabNavigator()
 
+const styles = StyleSheet.create({
+  iconStyle: {
+    height: 30,
+    width: 30
+  }
+})
+
 function TabsScreens() {
   return (
     <Tabs.Navigator 
       initialRouteName="Rechercher"
+      screenOptions={({ route }) => ({
+        tabBarIcon: () => {
+          if (route.name === 'Rechercher') {
+            return <Image style={styles.iconStyle} source={require('../assets/ic_search.png')}/>
+          } else if (route.name === 'Favoris') {
+            return <Image style={styles.iconStyle} source={require('../assets/ic_favorite.png')}/>
+          }
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'blue',
+        inactiveTintColor: 'gray',
+      }}
     >
-      <Tabs.Screen name="Rechercher" component={SearchStackScreen}/>
-      <Tabs.Screen name="Favoris" component={FavorisStackScreen}/>
+      <Tabs.Screen 
+        name="Rechercher" 
+        component={SearchStackScreen}
+      />
+      <Tabs.Screen 
+        name="Favoris" 
+        component={FavorisStackScreen}
+      />
     </Tabs.Navigator>
   )
 }
